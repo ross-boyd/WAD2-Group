68aac0f337537bab21cf15d019acc0d31446ee84
Ross <rssbyd@outlook.com>
2019-02-28 17:37:59 +0000

    Vote view

--- ratemydog/bestboy/views.py
+++ ratemydog/bestboy/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, 'home.html')
+
+
+@login_required
+def vote(request):
+    return render(request, 'vote.html')
