949dff5caf1054479910e638bdb44bbf0d8464a6
Ross <rssbyd@outlook.com>
2019-02-28 17:54:41 +0000

    URLs for vote

--- ratemydog/ratemydog/urls.py
+++ ratemydog/ratemydog/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
     # path('users/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
+    path('vote/', include('bestboy.urls')),
 ]
