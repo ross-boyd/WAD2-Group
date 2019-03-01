69e289e71766adee397253978c55f9b077073e11
Leo <leonard@koschitzke.de>
2019-03-01 02:03:16 +0000

    Added image field and changed requirements.txt

--- ratemydog/bestboy/models.py
+++ ratemydog/bestboy/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
