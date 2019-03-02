a88d11465d2edf36e559865fe3bae05f257c931f
Leo <leonard@koschitzke.de>
2019-03-02 10:55:59 +0000

    Modified population script

--- ratemydog/populate_dogs.py
+++ ratemydog/populate_dogs.py
@@ -10,19 +10,20 @@ import random
 def populate():
     directory = os.getcwd() + "\\bestboy\\media\\bestboy\\dog_pics"
     names = ["Leo", "Ross", "Bruce", "Danny"]
+    breeds = ["German Shepherd", "Dachshund", "Dobermann"]
     for file in os.listdir(directory):
         if file.endswith(".jpg"):
-            save_dog(random.choice(names), "German Shepherd", random.randint(1,10), directory + file)
+            save_dog(random.choice(names), random.choice(breeds), 
+                     random.randint(1, 10), directory + file)
 
 
 def save_dog(name, breed, rating, picture):
     print(picture)
-    d = Dog.objects.get_or_create(name=name)[0]
+    d = Dog.objects.get_or_create(name=name, picture=picture)[0]
     d.breed = breed
     d.rating = rating
-    d.picture = picture
-
     d.save()
+    
     return d
 
 
