3d310ae4958cf0ec25dd5b6b6338a474ad30a66e
Daniel Hislop <daniel.hislop14@gmail.com>
2019-03-06 13:14:17 +0000

    Display 10 Dogs
    Currently displays top 10 dogs, using home-table. No additional information shown, Size and width of each image not altered

--- ratemydog/bestboy/views.py
+++ ratemydog/bestboy/views.py
@@ -8,9 +8,18 @@ from bestboy.models import Dog
 
 
 def index(request):
-        dogs = Dog.objects.all().order_by('-rating')[:10]
-        dog_id = str(dogs[0].dog_id)
-        return render(request, 'home.html', {'dog_id': dog_id})
+        dog = Dog.objects.all().order_by('-rating')[:10]
+        return render(request, 'home.html', {'dog_id0': str(dog[0].dog_id),
+                                             'dog_id1': str(dog[1].dog_id),
+                                             'dog_id2': str(dog[2].dog_id),
+                                             'dog_id3': str(dog[3].dog_id),
+                                             'dog_id4': str(dog[4].dog_id),
+                                             'dog_id5': str(dog[5].dog_id),
+                                             'dog_id6': str(dog[6].dog_id),
+                                             'dog_id7': str(dog[7].dog_id),
+                                             'dog_id8': str(dog[8].dog_id),
+                                             'dog_id9': str(dog[9].dog_id),
+                                             })
 
 
 @login_required
