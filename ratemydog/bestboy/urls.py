0d62634e4bd46f3aa705b95bf7e9c2abe313b1cb
Bruce Kerr <bk@brucekerr.net>
2019-03-19 16:36:40 +0000

    comment out profile path
    #

--- ratemydog/bestboy/urls.py
+++ ratemydog/bestboy/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('vote/', views.vote, name='vote'),
     path('upload/', views.upload, name='upload'),
-    path('profile/', views.profile, name='profile')
+    #path('profile/', views.profile, name='profile')
 ]
