127a51f4310df9653cae6d1ace0e863c23ab363b
Leo <leonard@koschitzke.de>
2019-03-20 15:18:36 +0000

    Updated forms

---
+++ ratemydog/bestboy/choices.py
@@ -0,0 +1,7 @@
+BREED_CHOICES = (
+    ('UK', 'Unkown'),
+    ('GS', 'Leo has no nuts'),
+    ('DM', 'Dobermann'),
+    ('DH', 'Dachshund'),
+)
+
--- ratemydog/bestboy/forms.py
+++ ratemydog/bestboy/forms.py
@@ -1,18 +1,38 @@
 from django import forms
 from bestboy.models import Dog, Rating
+import os
+from bestboy.choices import *
 
 
 class RatingForm(forms.ModelForm):
-    score = forms.CharField(max_length=128, required=False)
-    text = forms.CharField(widget=forms.Textarea, required=False)
+    text = forms.Textarea()
 
     class Meta:
         model = Rating
-        fields = ('score', 'text',)
+        fields = ('text',)
 
 
 class UploadForm(forms.ModelForm):
+    ALLOWED_TYPES = ['jpg', 'jpeg', 'png', 'gif']
+
+    name = forms.CharField(max_length=100)
+    breed = forms.ChoiceField(choices=BREED_CHOICES)
+    picture = forms.ImageField()
+
     class Meta:
         model = Dog
-        fields = ('dog_id', 'name', 'owner',
-                  'score', 'picture', 'votes',)
+        fields = ('name','breed','picture',)
+
+    def clean_picture(self):
+        image = self.cleaned_data.get('picture', None)
+        
+        if not image:
+            raise forms.ValidationError('Missing image file')
+        try:
+            extension = os.path.splitext(image.name)[1][1:].lower()
+            if extension in self.ALLOWED_TYPES:
+                return image
+            else:
+                raise forms.ValidationError('File types is not allowed')
+        except Exception as e:
+            raise forms.ValidationError('Can not identify file type')
